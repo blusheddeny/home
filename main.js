e43cc6eafffe1129920f3e3d2854a25842668f97
Ingrid <girnid4@gmail.com>
2022-06-25 17:51:40 -0500

    Add js

---
+++ main.js
@@ -0,0 +1,7 @@
+const url = 'https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/628194/es/0700/true/BIE/2.0/40ffedb5-73a5-fe49-7aee-2d740b3c90ed?type=json'
+fetch(url,{ method:'GET', headers:{
+    'Content-Type': 'application/json',
+}})
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.log(err))
