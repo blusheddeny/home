151e59f4c1df524a3e8dcd8cc07e40b6b7cf5886
Ingrid <girnid4@gmail.com>
2022-06-25 22:50:09 -0500

    fix problem with function

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const url = 'https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/628194/es/0700/true/BIE/2.0/40ffedb5-73a5-fe49-7aee-2d740b3c90ed?type=json'
-const response = await fetch(url)
-const data = await response.json()
-console.log(data)
+async function getData(){
+    const url = 'https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/628194/es/0700/true/BIE/2.0/40ffedb5-73a5-fe49-7aee-2d740b3c90ed?type=json'
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+}
